fe05a785d357692958c7bc8e82746779b8cbe9ed
S. Dhyanesh <dhyan130709@gmail.com>
2023-10-20 20:52:10 +0530

    (Refractor) Create EnemySpaceship() to enable image change
    - The class contains the image, its rect and the slot factors for the image
    - The slot factors are values that are multiplied with the width and height of the image rect to get the appropriate coordinates of the firing slots in the image
    - This multiplication is done in the get_slot_coords() method

--- game/sprites/enemy.py
+++ game/sprites/enemy.py
@@ -1,7 +1,28 @@
 import pygame
-from typing import List, Union
+from typing import List, Tuple, Union
 
 from .bullet import Bullet, BulletGroup
+
+
+class EnemySpaceship:
+    def __init__(self, 
+                 img: pygame.Surface,
+                 slot_factors: List[Tuple[float, float]] = None) -> None:
+        self.img = img
+        self.slot_factors = slot_factors if slot_factors is not None else []
+        self.rect = self.img.get_rect()
+        
+    @property
+    def no_of_slots(self) -> int:
+        return len(self.slot_factors)
+        
+    def get_slot_coords(self, slot_idx: int):
+        slot_factors = self.slot_factors[slot_idx]
+        return (self.rect.width * slot_factors[0], self.rect.height * slot_factors[1])
+    
+    def update(self, surface: pygame.Surface) -> None:
+        surface.blit(self.img, self.rect)
+
 
 class Enemy:
     def __init__(self, display: pygame.Surface) -> None:
@@ -11,32 +32,38 @@ class Enemy:
         self.__load()
         
     def __load(self) -> None:
-        self.image = pygame.image.load("assets/images/enemy.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image , (100 , 75))
-        self.rect = self.image.get_rect()
+        spaceship_image = pygame.image.load("assets/images/enemy.png").convert_alpha()
+        spaceship_image = pygame.transform.scale(spaceship_image , (100 , 75))
+        self.spaceship = EnemySpaceship(spaceship_image)
+        self.spaceship.slot_factors = [
+            (0.22, 0.5),
+            (0.5, 0.5),
+            (0.78, 0.5)
+        ]
         
         self.bullet_img = pygame.image.load("assets/images/enemy_bullet.png").convert_alpha()
         self.bullet_img = pygame.transform.rotate(self.bullet_img, 180)
         self.bullet_img = pygame.transform.scale(self.bullet_img , (40, 40))
         self.bullet_group = BulletGroup(self.display, lambda bullet: bullet.rect.top > self.display.get_height())
         
+    @property
+    def rect(self) -> pygame.Rect: 
+        return self.spaceship.rect
+    
+    @rect.setter
+    def rect(self, new_rect: pygame.Rect) -> None: 
+        self.spaceship.rect = new_rect
+        
     def update(self) -> None:
         self.bullet_group.update_all()
-        self.display.blit(self.image, self.rect)
-        
-    def _determine_slot_coordinates(self, slot: int) -> float:
-        slot1_x = self.rect.x + self.rect.width * 0.22
-        slot2_x = self.rect.x + self.rect.width * 0.5
-        slot3_x = self.rect.x + self.rect.width * 0.78
-        slot_coordinates = [slot1_x, slot2_x, slot3_x]
-        return slot_coordinates[slot]
+        self.spaceship.update(self.display)
 
-    def __fire_bullet(self, slot: int = 1) -> None:
-        if slot not in [0, 1, 2]:
-            raise ValueError(f"Slot must range between 0 and 2, recieved {slot}.")
+    def __fire_bullet(self, slot: int = 0) -> None:
+        if slot > self.spaceship.no_of_slots - 1:
+            raise ValueError('Slot index out of range.')
+            
         bullet = Bullet(self.display, self.bullet_img, self.bullet_vel)
-        bullet_top = self.rect.centery + 10
-        bullet.rect.midtop = (self._determine_slot_coordinates(slot), bullet_top)
+        bullet.rect.midtop = self.spaceship.get_slot_coords(slot)
         bullet.fire()
         self.bullet_group.add(bullet)
         
