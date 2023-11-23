8bacea29268a6e59527964a9aed81ff245574fd8
S. Dhyanesh <dhyan130709@gmail.com>
2023-11-23 16:26:07 +0530

    (Refractor) Remove __iter__ for EnemyManager()

--- game/game.py
+++ game/game.py
@@ -31,7 +31,7 @@ class Game:
         
     def check_bullets(self) -> None:
         for bullet in reversed(self.player.bullet_group.bullet_list):
-            for enemy in self.enemy_manager:
+            for enemy in self.enemy_manager.enemies_list:
                 offset = (enemy.rect.x - bullet.rect.x, enemy.rect.y - bullet.rect.y)
                 if bullet.mask.overlap(enemy.mask, offset) and enemy.is_alive:
                     enemy.health -= bullet.damage
