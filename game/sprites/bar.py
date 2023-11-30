bf0984ad8d3c9940700ac8d96afc89e1c17df7f8
S. Dhyanesh <dhyan130709@gmail.com>
2023-11-30 19:15:21 +0530

    (Refractor) Improve Bar()
    - Add property and setters for width and height
    - Change color according to value

--- game/sprites/bar.py
+++ game/sprites/bar.py
@@ -1,10 +1,12 @@
 import pygame
-from typing import Union, Tuple
+from typing import Union, Tuple, List
+
+color = Union[pygame.Color, str, Tuple[int, int, int]]
 
 class Bar:
     def __init__(self, 
-                 fill_color: Union[str, Tuple[int, int, int]] = "green",
-                 empty_color: Union[str, Tuple[int, int, int]] = "black",
+                 fill_color: color = "green",
+                 empty_color: color = "black",
                  width: int = 100,
                  height: int = 20,
                  orient: str = "h",
@@ -13,7 +15,8 @@ class Bar:
                  to: int = 100,
                  value: int = 30,
                  outline_width: int = 0,
-                 outline_color: Union[str, Tuple[int, int, int]] = "red"):
+                 outline_color: color = "red",
+                 colors: List[Tuple[int, color, color]] = None):
         self.fill_color = fill_color
         self.empty_color = empty_color
         self.from_ = from_
@@ -23,27 +26,49 @@ class Bar:
         self.outline_color = outline_color
         self.orient = orient
         self.flipped = flipped
+        self.colors = [] if colors is None else colors
         
         self.rect = pygame.Rect(0, 0, width, height)
         
+    @property
+    def width(self): return self.rect.width
+    
+    @width.setter
+    def width(self, val: int): self.rect.width = val
+    
+    @property
+    def height(self): return self.rect.height
+    
+    @height.setter
+    def height(self, val: int): self.rect.height = val
+        
     def render_outline(self, surface: pygame.Surface) -> None:
+        outline_color = self.get_colors()[1]
         if self.outline_width > 0:
-            pygame.draw.rect(surface, self.outline_color, self.rect, self.outline_width)
+            pygame.draw.rect(surface, outline_color, self.rect, self.outline_width)
             
     def render_horiontal(self, surface: pygame.Surface) -> None:
+        fill_color = self.get_colors()[0]
         pygame.draw.rect(surface, self.empty_color, self.rect)
-        fill_width = min(self.rect.width * ((self.value - self.from_) / self.to), self.rect.width)
-        pygame.draw.rect(surface, self.fill_color, (self.rect.right - fill_width if self.flipped else self.rect.x, self.rect.y, fill_width, self.rect.height))
+        fill_width = min(self.width * ((self.value - self.from_) / self.to), self.width)
+        pygame.draw.rect(surface, fill_color, (self.rect.right - fill_width if self.flipped else self.rect.x, self.rect.y, fill_width, self.height))
         self.render_outline(surface)
         
     def render_vertical(self, surface: pygame.Surface) -> None:
+        fill_color = self.get_colors()[0]
         pygame.draw.rect(surface, self.empty_color, self.rect)
-        fill_height = min(self.rect.height * ((self.value - self.from_) / self.to), self.rect.height)
-        pygame.draw.rect(surface, self.fill_color, (self.rect.x, self.rect.y if self.flipped else (self.rect.bottom - fill_height), self.rect.width, fill_height))
+        fill_height = min(self.height * ((self.value - self.from_) / self.to), self.height)
+        pygame.draw.rect(surface, fill_color, (self.rect.x, self.rect.y if self.flipped else (self.rect.bottom - fill_height), self.width, fill_height))
         self.render_outline(surface)
     
     def flip_dimensions(self) -> None:
         self.width, self.height = self.height, self.width
+        
+    def get_colors(self) -> Tuple[color, color]:
+        for max_perc, fill_color, outline_color in reversed(self.colors):
+            if self.value/self.to*100 < max_perc:
+                return fill_color, outline_color
+        return self.fill_color, self.outline_color
     
     def draw(self, surface: pygame.Surface) -> None:
         if self.orient.lower() in ["h", "horizontal"]:
