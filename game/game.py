b7baa4865b4de078d871c85cc6bacd0bdf6cb51a
S. Dhyanesh <dhyan130709@gmail.com>
2023-12-28 15:39:10 +0530

    Add background music

--- game/game.py
+++ game/game.py
@@ -11,17 +11,10 @@ class Game:
         self.display = display
         self.game_state_manager = game_state_manager
         
+        self.load_sounds()
         self._load_game_objects()
         
     def _load_game_objects(self):
-        self.sounds = {
-            'damage': 'assets/sounds/damage.mp3',
-            'player_gunshot': 'assets/sounds/player_gunshot.mp3',
-            'enemy_gunshot': 'assets/sounds/enemy_gunshot.mp3',
-            'game_over': 'assets/sounds/game_over.mp3',
-        }
-        self.sounds: Dict[str, pygame.mixer.Sound] = {name: pygame.mixer.Sound(path) for name, path in self.sounds.items()}
-        
         self.player = Player()
         self.player.rect.midbottom = (self.display.get_width() / 2, self.display.get_height() - 50)
         self.player.durability = 400
@@ -31,6 +24,21 @@ class Game:
         self.enemy_manager.durability_probs = list(range(30, 50))*3 + list(range(60, 90))*2 + list(range(130, 180)) + list(range(180, 240, 5))
         self.enemy_manager.enemy_gunshot_sound = self.sounds['enemy_gunshot']
         # self.enemy_manager.debug = True
+        
+    def load_sounds(self):
+        self.sounds = {
+            'damage': 'assets/sounds/damage.mp3',
+            'player_gunshot': 'assets/sounds/player_gunshot.mp3',
+            'enemy_gunshot': 'assets/sounds/enemy_gunshot.mp3',
+            'game_over': 'assets/sounds/game_over.mp3',
+        }
+        self.sounds: Dict[str, pygame.mixer.Sound] = {name: pygame.mixer.Sound(path) for name, path in self.sounds.items()}
+        
+        self.bg_music_path = 'assets/sounds/bg_music.mp3'
+        self.bg_music_full_volume = 0.5
+        pygame.mixer.music.load(self.bg_music_path)
+        pygame.mixer.music.play()
+        pygame.mixer.music.set_volume(self.bg_music_full_volume)
         
     def handle_event(self, event: pygame.event.Event) -> None:
         if event.type == pygame.KEYDOWN:
@@ -57,19 +65,21 @@ class Game:
                     
     def handle_game_over(self) -> None:
         if self.player.health <= 0:
-            self.sounds['game_over'].play()
-            time.sleep(1)
             self.trigger_game_over("- Player Killed")
         for enemy in self.enemy_manager.enemies:
             if enemy.rect.centery > self.display.get_height() and enemy.health > 0:
                 if self.enemy_manager.debug:
                     print(f"Invaded: {enemy.label_text}")
-                self.sounds['game_over'].play()
-                time.sleep(1)
                 self.trigger_game_over("- Enemy has invaded the planet")
                 break
                         
     def trigger_game_over(self, msg: str) -> None:
+        for sound in self.sounds.values():
+            sound.stop()
+        pygame.mixer.music.set_volume(0.1)
+        self.sounds['game_over'].play()
+        time.sleep(1.5)
+        pygame.mixer.music.set_volume(self.bg_music_full_volume)
         self.game_state_manager.set_current_state("game_over")
         reason_msg = self.game_state_manager.get_state_by_name("game_over").obj.reason
         reason_msg.text = msg
